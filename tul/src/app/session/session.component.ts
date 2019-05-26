17e6ae8a20213ce1ad852e314cff2f120a955aed
John Walters <johnwalters@Mac-mini.local>
2019-05-25 23:14:42 -0700

    added timer to session component

--- tul/src/app/session/session.component.ts
+++ tul/src/app/session/session.component.ts
@@ -16,6 +16,11 @@ export class SessionComponent implements OnInit {
   exersizeSession: ExersizeSession;
   exersizeName: string;
 
+  timerCounter: number;
+  timerRef;
+  timerIsRunning = false;
+  timerText = 'Start';
+
   constructor(
     private exersizeService: ExersizeService,
     private route: ActivatedRoute,
@@ -31,6 +36,35 @@ export class SessionComponent implements OnInit {
 
   save() {
     this.exersizeService.addWorkout(this.workout);
+  }
+
+  startTimer() {
+    this.timerIsRunning = !this.timerIsRunning;
+    if (this.timerIsRunning) {
+      this.timerText = 'Stop';
+      const startTime = Date.now() - (this.timerCounter || 0);
+      this.timerRef = setInterval(() => {
+        this.timerCounter = Date.now() - startTime;
+      });
+    } else {
+      this.timerText = 'Resume';
+      this.exersizeSession.TimeUnderLoadSeconds = parseInt((this.timerCounter / 1000).toString(), 10) + 1;
+      this.save();
+      clearInterval(this.timerRef);
+    }
+  }
+
+  clearTimer() {
+    this.timerIsRunning = false;
+    this.timerText = 'Start';
+    this.timerCounter = undefined;
+    this.exersizeSession.TimeUnderLoadSeconds = 0;
+    this.save();
+    clearInterval(this.timerRef);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.timerRef);
   }
 
 }
